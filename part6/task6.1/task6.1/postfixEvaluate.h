d32803c83b630e3b40307b28553643fdd1548f2d
Matvey <matveynikola01@mail.ru>
2019-12-10 21:15:00 +0300

    add task 6.1

---
+++ part6/task6.1/task6.1/postfixEvaluate.h
@@ -0,0 +1,4 @@
+#pragma once
+
+int postfixExpressionEvaluator(char* string);
+int toNumber(int* arrayNumber, int numberSize);
---
+++ part6/task6.1/task6.1/task6.1.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+#include "test.h"
+#include "postfixEvaluate.h"
+
+int main()
+{
+    if (!test())
+	{
+		printf("programm work incorrectly");
+		return -1;
+	} 
+	char string[30] = {};
+
+	printf("Enter the expression in postfix form: ");
+	fgets(string, 30, stdin);
+	printf("Result is %i", postfixExpressionEvaluator(string));
+	
+	return 0;
+}
