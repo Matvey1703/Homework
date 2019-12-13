e038968e866154bef340dfbb03f7cf7f7da68ebb
Matvey <matveynikola01@mail.ru>
2019-12-13 20:15:20 +0300

    fixed task6.2

--- part6/task6.2/task6.2/stack.h
+++ part6/task6.2/task6.2/stack.h
@@ -2,8 +2,18 @@
 
 struct StackElement;
 struct Stack;
+
+// Check if stack is empty.
 bool isEmpty(Stack* stack);
+
+// Return new Stack.
 Stack* create();
+
+// Add new element in head of stack
 void push(int value, Stack* stack);
+
+// Delete element fron head of stack.
 int pop(Stack* stack);
+
+// Delete stack.
 void deleteStack(Stack* stack);
