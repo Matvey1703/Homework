1481a64fee5fa6c2e3305b1d6f9b3fa77e17aebc
Matvey <matveynikola01@mail.ru>
2019-12-13 19:50:29 +0300

    delete task6.2 from branch task6.3

--- part6/task6.2/task6.2/stack.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-struct StackElement;
-struct Stack;
-bool isEmpty(Stack* stack);
-Stack* create();
-void push(int value, Stack* stack);
-int pop(Stack* stack);
-void deleteStack(Stack* stack);
