1653fb99cf8060fc96bbd8c04832f86bea9ede21
Matvey <matveynikola01@mail.ru>
2019-12-16 14:38:30 +0300

    add task12.1

---
+++ part12/Task12.1.1/Task12.1.1/kmp.cpp
@@ -0,0 +1,71 @@
+#include <string.h>
+
+#include "kmp.h"
+
+void prefixFunction(char* string, int* pi)
+{
+	int stringSize = strlen(string);
+	int maxPrefix = 0; // Maximum length of matching prefix and postfix.
+	int stringIndex = 1;
+
+	pi[0] = 0;
+
+	while (stringIndex < stringSize)
+	{
+		if (string[stringIndex] == string[maxPrefix])
+		{
+			pi[stringIndex] = maxPrefix + 1;
+			stringIndex++;
+			maxPrefix++;
+		}
+		else
+		{
+			if (maxPrefix == 0)
+			{
+				pi[stringIndex] = 0;
+				stringIndex++;
+			}
+			else
+			{
+				maxPrefix = pi[maxPrefix - 1];
+			}
+		}
+	}
+}
+
+int finder(char* subString, char* string)
+{
+	int subStringSize = strlen(subString);
+	int stringSize = strlen(string);
+	int stringIndex = 0;
+	int subStringIndex = 0;
+	int* pi = new int[subStringSize] {};
+
+
+
+	prefixFunction(subString, pi);
+
+	while (stringIndex <= stringSize - subStringSize)
+	{
+		while (string[stringIndex] == subString[subStringIndex])
+		{
+			if (subStringIndex == subStringSize - 1)
+			{
+				return stringIndex - subStringSize + 1;
+			}
+			stringIndex++;
+			subStringIndex++;
+		}
+		if (subStringIndex == 0)
+		{
+			stringIndex++;
+		}
+		else
+		{
+			subStringIndex = pi[subStringIndex - 1];
+		}
+	}
+
+	delete[] pi;
+	return -1;
+}
---
+++ part12/Task12.1.1/Task12.1.1/kmp.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// Fill pi-array for string.
+void prefixFunction(char* string, int* pi);
+
+// Position of the first occurrence of the substruct in struct (return -1 if substruct did not found)).
+int finder(char* subString, char* string);
